b4a9cab36b51cc8ca9b9a67d4b7c82abf6f02df4
ippei94da <ippei94da@gmail.com>
2013-09-04 20:30:11 +0900

    small revision

--- Gemfile
+++ Gemfile
@@ -11,4 +11,5 @@ group :development do
   gem "jeweler", "~> 1.8.4"
   #gem "rcov", ">= 0"
   gem "simplecov", ">= 0"
+  gem "test-unit", ">= 2.5.5"
 end
--- test/test_gitan.rb
+++ test/test_gitan.rb
@@ -1,7 +1,18 @@
 require 'helper'
 
-class TestGitan < Test::Unit::TestCase
-  def test_something_for_real
-    #flunk "hey buddy, you should probably rename this file and start testing for real"
+#class TestGitan < Test::Unit::TestCase
+#  def test_something_for_real
+#    #flunk "hey buddy, you should probably rename this file and start testing for real"
+#  end
+#end
+
+#
+gem "test-unit"
+require "test/unit"
+
+class SampleTest < Test::Unit::TestCase
+  def test_foo
+    assert_equal(1,1)
+    assert_equal(1,2)
   end
 end
